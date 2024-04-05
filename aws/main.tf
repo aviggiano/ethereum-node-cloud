7e284a3ae8567bd7974c94cad9aa1e545d68f3b0
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2024-04-05 17:08:05 -0300

    Update volume storage

--- aws/main.tf
+++ aws/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "this" {
-  name   = "${var.namespace}-sg"
+  name = "${var.namespace}-sg"
   ingress {
     cidr_blocks = [
       "0.0.0.0/0"
@@ -18,10 +18,15 @@ resource "aws_security_group" "this" {
 
 resource "aws_instance" "this" {
   ami           = var.ami_id
-  instance_type = "t3.micro"
+  instance_type = "m5.xlarge"
 
-  vpc_security_group_ids = [aws_security_group.this.id]
+  vpc_security_group_ids      = [aws_security_group.this.id]
   associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 1400 # https://ycharts.com/indicators/ethereum_chain_full_sync_data_size
+    volume_type = "gp3"
+  }
 
   tags = {
     Name = "${var.namespace}-instance"
